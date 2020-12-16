555b13a0e5362c5a1de346d4b6d0669c8f71a268
androidpycan <75857651+anrooid@users.noreply.github.com>
2020-12-16 20:20:31 +0330

    First commit

---
+++ app/src/main/java/com/example/pycanmessenger/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.pycanmessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // app id = HRW78F396y8HrbHQI9uIPe9tk2032E8EMed0q12k
+        // client key  = HUBUNIcnJ9nUtGL76GZAZ3glWMKYYTsO9USf4zUC
+        // server = https://parseapi.back4app.com/
+    }
+}
