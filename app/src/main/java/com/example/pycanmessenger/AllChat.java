db4b09b0cf780d717ff19a3ede1f8e341314f7f3
anrooid <pycan.androoid@gmail.com>
2020-12-21 12:43:39 +0330

    update all chat

--- app/src/main/java/com/example/pycanmessenger/AllChat.java
+++ app/src/main/java/com/example/pycanmessenger/AllChat.java
@@ -20,7 +20,6 @@ import java.util.List;
 
 public class AllChat extends Fragment {
     RecyclerView myRecycler;
-    List<Item> mItem = new ArrayList<>();
     chatAdapter mAdapter;
 
     public AllChat(){
@@ -31,29 +30,9 @@ public class AllChat extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_all_chat, container, false);
-        myRecycler = view.findViewById(R.id.recycler_view);
-        mAdapter = new chatAdapter(mItem,getContext());
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
-        myRecycler.setLayoutManager(mLayoutManager);
-        myRecycler.setAdapter(mAdapter);
-        ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Chats");
-        parseQuery.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-
-            }
-        });
-        setData();
-
 
         return view;
 
     }
-    private void setData(){
-        mItem.add(new Item(R.drawable.cat, "mahdi","berim dasht" , "12:02"));
-        mItem.add(new Item(R.drawable.ic_launcher_background, "hfd","jgffdfhh dasht" , "06:02"));
-        mItem.add(new Item(R.drawable.mahdi, "recd","hgnngg dasht" , "14:02"));
 
-        mAdapter.notifyDataSetChanged();
-    }
 }
