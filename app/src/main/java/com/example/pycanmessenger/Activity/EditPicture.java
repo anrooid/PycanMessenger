8bc4c7c9de206c9f18142eb67244bb7d35a59445
anrooid <pycan.androoid@gmail.com>
2021-01-24 03:46:01 +0330

    taran : set Activity_edit_picture

---
+++ app/src/main/java/com/example/pycanmessenger/Activity/EditPicture.java
@@ -0,0 +1,16 @@
+package com.example.pycanmessenger.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pycanmessenger.R;
+
+public class EditPicture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_picture);
+    }
+}
