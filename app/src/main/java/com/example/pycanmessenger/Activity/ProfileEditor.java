0688166a2807e9e71eec3a950772ba05812ecc72
anrooid <pycan.androoid@gmail.com>
2021-01-28 01:11:46 +0330

    taran: set onClick for img editor

--- app/src/main/java/com/example/pycanmessenger/Activity/ProfileEditor.java
+++ app/src/main/java/com/example/pycanmessenger/Activity/ProfileEditor.java
@@ -2,23 +2,51 @@ package com.example.pycanmessenger.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.pycanmessenger.Models.BitMapHolder;
 import com.example.pycanmessenger.R;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
-public class ProfileEditor extends AppCompatActivity {
+public class ProfileEditor extends AppCompatActivity implements View.OnClickListener {
 
+    private ImageView imgRotate , imgFilter , imgFlip;
     private CropImageView img_edt ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_editor);
 
+        imgRotate = findViewById(R.id.img_rotate);
+        imgFilter = findViewById(R.id.img_filter);
+        imgFlip = findViewById(R.id.img_flip);
+
         img_edt = findViewById(R.id.img_edit);
         img_edt.setImageBitmap(BitMapHolder.getBitMapHolder().getBitmap());
 
-        BitMapHolder.getBitMapHolder().setBitmap(img_edt.getCroppedImage());
+        //BitMapHolder.getBitMapHolder().setBitmap(img_edt.getCroppedImage());
+
+        imgFilter.setOnClickListener(this);
+        imgRotate.setOnClickListener(this);
+        imgFlip.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.img_filter :
+                Toast.makeText(ProfileEditor.this , "this image is not available in this version", Toast.LENGTH_LONG).show();
+                break;
+            case  R.id.img_flip:
+                break;
+            case  R.id.img_rotate:
+                imgRotate.setRotation(imgRotate.getRotation()+90);
+                break;
+        }
     }
 }
